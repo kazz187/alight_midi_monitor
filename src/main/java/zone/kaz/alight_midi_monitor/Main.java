cb7d5e42ed3be20b456858dd8a691c35a34c5755
kazz187 <kazzone87@gmail.com>
2016-08-26 05:51:47 +0900

    Flip horizontal position of led.

--- src/main/java/zone/kaz/alight_midi_monitor/Main.java
+++ src/main/java/zone/kaz/alight_midi_monitor/Main.java
@@ -8,8 +8,8 @@ public class Main extends PApplet {
     private static Networking networking = new Networking(PORT);
     private static int X_NUM = 8;
     private static int Y_NUM = 50;
-    private static int WIDTH = 1600;
-    private static int HEIGHT = 800;
+    private static int WIDTH = 800;
+    private static int HEIGHT = 400;
 
     public static void main(String[] args) {
         networking.start();
@@ -36,7 +36,7 @@ public class Main extends PApplet {
                     color = color(buffer[index*3], buffer[index*3+1], buffer[index*3+2]);
                 }
                 fill(color);
-                ellipse((int) ((i + 0.5) * width / X_NUM), (int) ((j + 0.5) * height / Y_NUM), 10, 10);
+                ellipse((int) ((i + 0.5) * width / X_NUM), height - (int) ((j + 0.5) * height / Y_NUM), 10, 10);
             }
         }
 
